c4806ecedb63c25fc2909dd612e53b68fdf16a3c
Anoop <s437432@emirates.com>
2016-07-18 23:38:21 +0400

    Initial Commit

---
+++ Connect/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Connect
+//
+//  Created by Anoop on 7/18/16.
+//  Copyright © 2016 Anoop. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
